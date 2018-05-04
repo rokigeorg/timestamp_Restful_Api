953b45d6d1c01a2f0976c9b56735ed0f61405d48
Georg <rokita.georg@freenet.de>
2018-05-04 15:38:11 +0200

    init Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import json
+import time
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def getTimestamp():
+    data = {"time" : ""}
+    ts = time.time()
+    data["time"] = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
